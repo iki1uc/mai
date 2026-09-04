88171e2b57fb9a2f32ef77c7bfd571a2a522edbf
iki1uc <entertrainer@web.de>
2026-09-05 00:26:29 +0200

    Refactor ROM.js to implement Game object structure

--- rom.js
+++ rom.js
@@ -1,3 +1,20 @@
-// ROM.js
-const axis = A81_AXIS.build();
-ROM.mind = axis.pipe12;   // Mind-Schaltung
+const Game = {
+    minute: 0,
+    state: ROM.mind,      // aktueller Spielzug
+    gpu: gpuRAW,          // Ticks
+    schuss: Schuss,       // Aktionen
+    axis: axis,           // Spielfluss
+
+    tick() {
+        const t = this.gpu.render();   // Spielminute
+        this.minute = t.frame;
+
+        return {
+            minute: this.minute,
+            lauf: t.energy,
+            echo: t.echo,
+            wirkung: t.effect,
+            spielzug: this.state
+        };
+    }
+};
