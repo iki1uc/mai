2776e8eaa420a26bbaa5a8004c37362d1cad4c32
iki1uc <entertrainer@web.de>
2026-09-04 23:59:50 +0200

    Initialize axis with pump parameters

--- user.js
+++ user.js
@@ -1,2 +1,8 @@
-const axis = A81_AXIS.build();
+const axis = A81_AXIS.build({
+    wasser: PUMPE.wasser(81),
+    o2:     PUMPE.o2(81),
+    co2:    PUMPE.co2(81),
+    atom:   PUMPE.atom(81)
+});
+
 Pipeline9.useA81(axis);
