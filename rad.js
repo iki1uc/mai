338aeee13b51bc92461195306ce1c3f2855cd66e
iki1uc <entertrainer@web.de>
2026-09-05 00:45:48 +0200

    Refactor MassHWTranswarp.set function
    Refactor MassHWTranswarp.set to trigger ROM.on with parameters.

--- rad.js
+++ rad.js
@@ -1,5 +1,19 @@
-MassHWTranswarp.set(
-    qi,        // Masse
-    iqq,       // Geschwindigkeit
-    TimeHW.delta
-);
+ROM.on = function(name, content){
+    this.state = 1;          // immer ON
+    this.open = true;        // niemals OFF
+    this.name = name;        // Ereignisname
+    this.content = content;  // Übergabeinhalt
+    return this;
+};
+MassHWTranswarp.set = function(qi, iqq, delta){
+
+    // ROM.on wird automatisch ausgelöst
+    ROM.on("MassHWTranswarp.set", { qi, iqq, delta });
+
+    return {
+        qi,
+        iqq,
+        delta,
+        rom: ROM.state
+    };
+};
